53df145ca21908a666d5186d63f1f391c64b28f0
Mike Lollar <lollar.mchl@gmail.com>
2019-02-26 21:56:40 -0700

    Builds out initial spec for creating subscriptions

---
+++ spec/requests/subscriptions/create_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "[POST] /api/v1/subscriptions" do
+  subject(:create_subscription) { "/api/v1/subscriptions" }
+
+  context "when valid data is provided" do
+    let(:params) do
+      {
+        plan_id: 1,
+        shipping: {
+          name: "Bob Loblaw",
+          address: "123 N Main St",
+          zip_code: "00832",
+        },
+        billing: {
+          card_number: "4111111111111111",
+          exp_date: "07/19",
+          cvv: "100",
+          zip_code: "00835"
+        }
+      }
+    end
+    it "returns :created status" do
+      post create_subscription, params: params
+
+      expect(response).to have_http_status :created
+    end
+  end
+end
