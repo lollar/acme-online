3e8e7ce96a08277dff0aef27b13ca3a3b2af17fd
Mike Lollar <lollar.mchl@gmail.com>
2019-03-04 21:45:41 -0700

    Add some additional specs for exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
-  protected
+  rescue_from ::API::V1::UnprocessableEntity, with: :api_error_response
+  rescue_from ::API::V1::BadRequest, with: :api_error_response
 
   # Rails strong params work great for rails forms but I think they are sufficiently
   # lacking when implementing an API. This overrides the params to a Hash. Rails is just
   # being rude by making me call "to_unsafe_h" :(
   def params
     super.to_unsafe_h.deep_symbolize_keys
+  end
+
+  private
+
+  def api_error_response response
+    render response.result
   end
 end
--- spec/requests/subscriptions/create_spec.rb
+++ spec/requests/subscriptions/create_spec.rb
@@ -3,10 +3,11 @@
 require "rails_helper"
 
 RSpec.describe "[POST] /api/v1/subscriptions" do
-  subject(:create_subscription) { "/api/v1/subscriptions" }
+  subject(:create_subscription) { post "/api/v1/subscriptions", params: params }
+
+  let(:plan) { Plan.find_by name: "Bronze Box" }
 
   context "when valid data is provided", vcr: { cassette_name: "successful_post_subscriptions" } do
-    let(:plan) { Plan.find_by name: "Bronze Box" }
     let(:params) do
       {
         plan_id: plan.id,
@@ -26,12 +27,69 @@ RSpec.describe "[POST] /api/v1/subscriptions" do
     end
 
     it "returns :created status" do
-      post create_subscription, params: params
+      create_subscription
 
       data = JSON.parse(response.body).fetch("data", {}).deep_symbolize_keys
 
       expect(response).to have_http_status :created
       expect(data.keys).to contain_exactly :id, :customer_id, :plan_id, :due_on
+    end
+  end
+
+  context "when invalid data is provided", vcr: { cassette_name: "invalid_post_subscriptions" } do
+    let(:params) do
+      {
+        plan_id: plan.id,
+        customer: {
+          name: "bob loblaw",
+          address: "123 n main st",
+          zip_code: "00832",
+        },
+        billing: {
+          card_number: "4242424242424242",
+          expiration_month: "01",
+          expiration_year: "2024",
+          cvv: "101",
+          zip_code: "10045"
+        }
+      }
+    end
+
+    it "returns :created status" do
+      create_subscription
+
+      errors = JSON.parse(response.body).fetch("errors", "")
+
+      expect(response).to have_http_status :unprocessable_entity
+      expect(errors).to match /cvv/
+    end
+  end
+
+  context "when missing data is provided", vcr: { cassette_name: "missing_post_subscriptions" } do
+    let(:params) do
+      {
+        plan_id: plan.id,
+        customer: {
+          name: "Bob Loblaw",
+          address: "123 N Main St",
+          zip_code: "00832",
+        },
+        billing: {
+          card_number: "4242424242424242",
+          expiration_year: "2024",
+          cvv: "101",
+          zip_code: "10045"
+        }
+      }
+    end
+
+    it "returns :created status" do
+      create_subscription
+
+      errors = JSON.parse(response.body).fetch("errors", "")
+
+      expect(response).to have_http_status :bad_request
+      expect(errors).to match /expiration_month/
     end
   end
 end
